371d808f2215678cdcb42bd4cb113ec54ad8ed01
ChingHuaNing <89456025+AngelineChing@users.noreply.github.com>
2021-08-25 16:08:59 +0800

    Fetch Data Successfully

---
+++ src/components/DisplayUsers.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+  import axios from "axios";
+
+  export default {
+    name: 'DisplayUsers',
+    data() {
+      return {
+        baseUrl: process.env.VUE_APP_BASE_URL,
+      }
+    },
+
+    methods: {
+      fetchData(){
+        axios.get("https://jsonplaceholder.typicode.com/users").then(response => {
+            console.log(response);
+        })
+      }
+    },
+
+    created(){
+      // Fetch Data
+      this.fetchData();
+    },
+  }
+</script>
