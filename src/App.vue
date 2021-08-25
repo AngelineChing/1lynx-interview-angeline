a29c72a01f8c07436a71680aceccf547270605a8
ChingHuaNing <89456025+AngelineChing@users.noreply.github.com>
2021-08-25 12:08:48 +0800

    Navigation added

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,118 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+  <div>
+    <!-- Drawer Action Button -->
+    <v-app-bar fixed class="shadow-none py-3 white-bg">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="nav-icon"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+    <!-- Drawer Navigation -->
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+
+      <!-- Account Info + Edit Button -->
+      <v-card
+        class="d-flex flex-row align-items-center shadow-none px-2 my-2 justify-content-between"
+        color="#6474E5"
+        tile>
+        <v-avatar>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          />
+        </v-avatar>
+        <div>
+          <v-card-title>
+            John wefewqrfeqwrf
+          </v-card-title>
+          <v-card-subtitle>
+            019-8288465
+          </v-card-subtitle>
+        </div>
+        <v-card-actions>
+          <v-btn
+            icon
+            @click.stop="drawer = !drawer"
+          >
+            <v-icon color="#fff">mdi-account-edit-outline</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <!-- Display All Users Button -->
+      <v-card class="shadow-none py-2">
+        <v-btn
+          @click.stop="drawer = !drawer"
+          @click="display"
+          color="#6474E5"
+          text
+          elevation="0"
+          width="100%"
+          tile
+        >
+          <v-icon>mdi-view-list</v-icon>
+          <v-card-text> Display All Users</v-card-text>
+        </v-btn>
+      </v-card>
+      <!-- <router-link to="{ path: '/EditProfile' }">
+        <v-btn>
+          Home
+          </v-btn>
+          </router-link> -->
+    </v-navigation-drawer>
+
+    <EditProfile v-model="edit"/>
+    <DisplayUsers v-model="display"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import EditProfile from './components/EditProfile';
+import DisplayUsers from './components/DisplayUsers';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    EditProfile,
+    DisplayUsers,
   },
 
-  data: () => ({
-    //
-  }),
+  data () {
+    return {
+      drawer: null,
+      edit: false,
+    }
+  },
+
+  methods: {
+    display() {
+      const path = `/DisplayUsers`
+      if (this.$route.path !== path) this.$router.push(path)
+    }
+  },
 };
 </script>
+
+<style>
+.white-bg {
+  background-color: transparent !important;
+}
+.nav-icon {
+  background-color: #6474E5;
+  color: white !important;
+}
+.v-card__title {
+  color: white;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  word-break: break-word;
+  display: -webkit-box !important;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  line-height: 2.8rem !important;
+  padding: 10px !important;
+}
+.v-card__subtitle {
+  color: white;
+  padding: 0 10px 15px !important;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+// import EditProfile from "./components/EditProfile";
+// import DisplayUsers from "./components/DisplayUsers";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Navigation route
+Vue.use(VueRouter);
+
+// const edit = { template: <EditProfile /> };
+// const display = { template: <DisplayUsers /> };
+
+// const routes = [
+//   { path: "/EditProfile", component: edit },
+//   { path: "/DisplayUsers", component: display },
+// ];
+
+// const router = new VueRouter({
+//   routes, // short for `routes: routes`
+// });
+
+// new Vue({
+//   vuetify,
+//   router,
+//   render: (h) => h(App),
+// }).$mount("#app");
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
